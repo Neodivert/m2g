fede7e7d3d8f82001bbe868bb88bc8dbb2853120
moisesjbc <moisesjbc@gmail.com>
2015-01-23 10:36:35 +0000

    Added auxiliar struct TextTexture.

---
+++ src/text/text_texture.hpp
@@ -0,0 +1,57 @@
+/***
+ * Copyright 2013 - 2015 Moises J. Bonilla Caraballo (Neodivert)
+ *
+ * This file is part of M2G.
+ *
+ * M2G is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * M2G is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with M2G.  If not, see <http://www.gnu.org/licenses/>.
+***/
+
+#ifndef TEXT_TEXTURE_HPP
+#define TEXT_TEXTURE_HPP
+
+#include <SDL2/SDL.h>
+
+namespace m2g {
+
+struct TextTexture
+{
+    SDL_Renderer* renderer;
+    SDL_Texture* texture;
+    const SDL_Rect textRect;
+
+    /***
+     * 1. Construction and destruction
+     ***/
+    TextTexture( SDL_Renderer* renderer,
+                 SDL_Surface* surface,
+                 const SDL_Rect& textRect );
+    TextTexture( const TextTexture& ) = delete;
+    ~TextTexture();
+
+
+    /***
+     * 2. Drawing
+     ***/
+    void draw( SDL_Rect dstRect ) const;
+
+
+    /***
+     * 3. Operators
+     ***/
+    TextTexture& operator = ( const TextTexture& ) = delete;
+};
+
+} // namespace m2g
+
+#endif // TEXT_TEXTURE_HPP
