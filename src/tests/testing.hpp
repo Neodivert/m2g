5d6e87862cf3a91ac52c69361b1f51912cb023cb
moisesjbc <moisesjbc@gmail.com>
2015-02-02 11:12:15 +0000

    Added missing include guards to testing.hpp

--- src/tests/testing.hpp
+++ src/tests/testing.hpp
@@ -1,2 +1,7 @@
+#ifndef TESTING_HPP
+#define TESTING_HPP
+
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include <catch.hpp>
+
+#endif // TESTING_HPP
