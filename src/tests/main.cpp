59abafc1c93c202bf8c8a95573e9f8f0a2859e55
moisesjbc <moisesjbc@gmail.com>
2015-02-23 19:01:30 +0000

    Added a test main.cpp file with a main() function.

---
+++ src/tests/main.cpp
@@ -0,0 +1,28 @@
+/***
+ * Copyright 2013 - 2015 Moises J. Bonilla Caraballo (Neodivert)
+ *
+ * This file is part of M2G.
+ *
+ * M2G is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * M2G is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with M2G.  If not, see <http://www.gnu.org/licenses/>.
+***/
+
+#define CATCH_CONFIG_RUNNER
+#include <catch.hpp>
+
+int main( int argc, char* const argv[] )
+{
+  int testsResult = Catch::Session().run( argc, argv );
+
+  return testsResult;
+}
